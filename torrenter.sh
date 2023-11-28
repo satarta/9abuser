12c18e4044d01215a17bfc6c71e937762efc54db
satarta <68680921+satarta@users.noreply.github.com>
2023-11-28 12:18:06 +0300

    Update torrenter.sh

--- torrenter.sh
+++ torrenter.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
-#записываем текст из буфера обмена в файл
-xclip -o clip > temp.txt
-#берем ip и title
-ip=$(grep "<IP_Address>" temp.txt|sed "s/<IP_Address>//; s/<\/IP_Address>//")
-title=$(grep "<Title>" temp.txt|sed "s/<Title>//; s/<\/Title>//")
-#выводит ip и title, можно убрать
-echo $ip
-echo $title
-#подставляем ip и title в шаблон (нужно его иметь)
-cat abusetitle |sed "s/IPADDRESS/$ip/"|sed "s/TITLE/$title/" > end
-#дописываем наш скопированный текст в конец конечного файла
-xclip -o clip >> end
-#закрываем noformat
-echo {noformat} >> end
-#копируем файл в буфер обмена
-xclip -sel clip end
-~
+
+# Извлечение IP и заголовка из буфера обмена
+ip=$(xclip -o clip | grep -oP '<IP_Address>\K.*?(?=<\/IP_Address>)')
+title=$(xclip -o clip | grep -oP '<Title>\K.*?(?=<\/Title>)')
+
+# Замена шаблонов в файле abusetitle и сохранение результата во временный файл
+sed "s/IPADDRESS/$ip/; s/TITLE/$title/" ~/texts/abusetitle > temp_end
+
+# Добавление {noformat} и контента из буфера обмена в конечный файл
+xclip -o clip >> temp_end
+echo "{noformat}" >> temp_end
+# Копирование конечного файла в буфер обмена
+xclip -sel clip temp_end
+
+# Удаление временных файлов
+rm temp_end
